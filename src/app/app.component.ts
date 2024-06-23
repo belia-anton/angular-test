748724bcf551f15d8db1e2cb23d31f35deff4e02
Belia Anton <belja.anton@gmail.com>
2024-06-23 16:03:40 +0300

    github pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import 'flex-layout-system';
 import { CounterCardComponent } from './counter-card/counter-card.component';
 import { CardComponent } from './card/card.component';
@@ -7,7 +8,7 @@ import { CardComponent } from './card/card.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CounterCardComponent, CardComponent],
+  imports: [CommonModule, RouterOutlet, CounterCardComponent, CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
